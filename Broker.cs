57810d343ae157d7434d44291f2a3f0bdb6b4959
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-08-22 20:20:02 +0530

    Add files via upload

---
+++ Broker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Code_First
+{
+    class Broker
+    {
+        public int BrokerId { get; set; }
+        public string BrokerCode { get; set; }
+        public string BrokerName { get; set; }
+    }
+}
---
+++ MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Code_First
+{
+    class MyDbContext :DbContext
+    {
+        public MyDbContext() : base("name=MDSConString") { }
+
+        public virtual DbSet<Broker> Brokers { get; set; }
+    }
+}
